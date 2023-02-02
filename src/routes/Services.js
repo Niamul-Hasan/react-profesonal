388b93b4fd3374e917a4c95b02a55fa537eda6e3
Niamul Hasan Zead <zead.ece2014@gmail.com>
2023-02-02 18:09:14 +0600

    project is on the way

---
+++ src/routes/Services.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Services = () => {
+  return (
+    <>
+      <h2>Services page</h2>
+
+      <Link to="/web-design">Web design</Link>
+      <Link to='/web-dev'>Web Dev</Link>
+      <Link to="/seo">Seo</Link>
+    </>
+  );
+};
+
+export default Services;
